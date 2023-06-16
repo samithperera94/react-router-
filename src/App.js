719c89d080c4943b513d8d8546d8d153cddb7c72
samithperera94 <samiththilanka94@gmail.com>
2023-06-16 13:19:06 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import RootLayout from "./pages/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductPage />,
+      },
+    ],
+  },
+  // {
+  //   path: "/aidim",
+  //   element: ,
+  //   children: [
+
+  //   ],
+  // },
+  // {
+  //   path:'/',
+  //   element:<HomePage/>
+  // },
+  // {
+  //   path:'/products',
+  //   element:<ProductPage/>
+  // }
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
